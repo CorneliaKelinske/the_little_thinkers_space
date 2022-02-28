16dfb9132693c57087408fb8e79f38c346ac6567
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-02-28 07:14:55 -0800

    add error options to upload controller delete function and move private functions out of upload controller into separate module

---
+++ lib/the_little_thinkers_space/upload_handler.ex
@@ -0,0 +1,60 @@
+defmodule TheLittleThinkersSpace.UploadHandler do
+  alias TheLittleThinkersSpace.{DataPath, UploadPathsHelper}
+  alias TheLittleThinkersSpace.Content.Upload
+
+  def store_upload(%{"upload" => %Plug.Upload{filename: filename, path: path}}, user_id) do
+    maybe_create_user_directory(user_id)
+    storage_path = "#{DataPath.set_data_path()}/#{user_id}/#{filename}"
+    with :ok <- File.cp(path, "#{storage_path}") do
+    {:ok, storage_path}
+    else
+      {:error, _} -> {:error, :file_not_saved}
+    end
+  end
+
+  def delete_upload(%{path: path}) do
+    delete_path = UploadPathsHelper.delete_path(DataPath.set_data_path)
+    full_delete_path = "#{delete_path}#{path}"
+    File.rm(full_delete_path)
+
+  end
+
+  defp maybe_create_user_directory(user_id) do
+    case File.exists?("#{DataPath.set_data_path()}/#{user_id}") do
+      false ->
+        File.mkdir("#{DataPath.set_data_path()}/#{user_id}")
+
+      true ->
+        :ok
+    end
+  end
+
+  def create_show_path(storage_path) do
+    UploadPathsHelper.show_path(storage_path)
+  end
+
+  def parse_upload_params(
+         %{
+           "title" => title,
+           "description" => description,
+           "orientation" => orientation,
+           "upload" => %Plug.Upload{content_type: content_type}
+         },
+         show_path
+       ) do
+    attrs = %{
+      "path" => show_path,
+      "title" => title,
+      "description" => description,
+      "orientation" => orientation,
+      "file_type" => content_type
+    }
+
+    {:ok, attrs}
+  end
+
+  def parse_upload_params(_, _) do
+    {:error, :file_not_uploaded}
+  end
+
+end
--- lib/the_little_thinkers_space_web/controllers/upload_controller.ex
+++ lib/the_little_thinkers_space_web/controllers/upload_controller.ex
@@ -1,6 +1,6 @@
 defmodule TheLittleThinkersSpaceWeb.UploadController do
   use TheLittleThinkersSpaceWeb, :controller
-  alias TheLittleThinkersSpace.{FileCompressor, Content, DataPath, UploadPathsHelper}
+  alias TheLittleThinkersSpace.{FileCompressor, Content, UploadHandler}
 
   alias TheLittleThinkersSpace.Content.Upload
   action_fallback TheLittleThinkersSpaceWeb.FallbackController
@@ -24,9 +24,9 @@ defmodule TheLittleThinkersSpaceWeb.UploadController do
 
     with :ok <- Bodyguard.permit(Upload, :create, user, upload),
          upload <- FileCompressor.compress_file(upload),
-         {:ok, storage_path} <- store_upload(upload, user.id),
-         {:ok, show_path} <- create_show_path(storage_path),
-         {:ok, attrs} <- parse_upload_params(upload, show_path),
+         {:ok, storage_path} <- UploadHandler.store_upload(upload, user.id),
+         {:ok, show_path} <- UploadHandler.create_show_path(storage_path),
+         {:ok, attrs} <- UploadHandler.parse_upload_params(upload, show_path),
          {:ok, upload} <- Content.create_upload(user, attrs) do
       conn
       |> put_flash(:info, "File uploaded successfully.")
@@ -46,6 +46,11 @@ defmodule TheLittleThinkersSpaceWeb.UploadController do
         conn
         |> put_flash(:error, "You are not allowed to do this!")
         |> redirect(to: Routes.page_path(conn, :home))
+
+      {:error, :file_not_saved} ->
+        conn
+        |> put_flash(:error, "File not saved, please try again!")
+        |> redirect(to: Routes.upload_path(conn, :new))
 
       {:error, :no_show_path} ->
         conn
@@ -91,66 +96,27 @@ defmodule TheLittleThinkersSpaceWeb.UploadController do
   def delete(conn, %{"id" => id}) do
     user = conn.assigns.current_user
 
-    with :ok <- Bodyguard.permit(Upload, :delete, user, id) do
-      upload = Content.get_upload!(id)
-      {:ok, _upload} = Content.delete_upload(upload)
-      delete_upload(upload)
+    with :ok <- Bodyguard.permit(Upload, :delete, user, id),
+      upload <- Content.get_upload!(id),
+      :ok <- UploadHandler.delete_upload(upload),
+      {:ok, _upload} <- Content.delete_upload(upload) do
 
       conn
       |> put_flash(:info, "Upload deleted successfully.")
       |> redirect(to: Routes.upload_path(conn, :index))
+    else
+      nil ->
+        conn
+        |> put_flash(:error, "File not found!")
+        |> redirect(to: Routes.upload_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Unable to delete file!")
+        |> redirect(to: Routes.upload_path(conn, :index))
+
     end
   end
 
-  defp store_upload(%{"upload" => %Plug.Upload{filename: filename, path: path}}, user_id) do
-    maybe_create_user_directory(user_id)
-    storage_path = "#{DataPath.set_data_path()}/#{user_id}/#{filename}"
-    File.cp(path, "#{storage_path}")
-    {:ok, storage_path}
-  end
 
-  defp delete_upload(%{path: path}) do
-    delete_path = UploadPathsHelper.delete_path(DataPath.set_data_path)
-    full_delete_path = "#{delete_path}#{path}"
-    File.rm(full_delete_path)
-
-  end
-
-  defp maybe_create_user_directory(user_id) do
-    case File.exists?("#{DataPath.set_data_path()}/#{user_id}") do
-      false ->
-        File.mkdir("#{DataPath.set_data_path()}/#{user_id}")
-
-      true ->
-        :ok
-    end
-  end
-
-  defp create_show_path(storage_path) do
-    UploadPathsHelper.show_path(storage_path)
-  end
-
-  defp parse_upload_params(
-         %{
-           "title" => title,
-           "description" => description,
-           "orientation" => orientation,
-           "upload" => %Plug.Upload{content_type: content_type}
-         },
-         show_path
-       ) do
-    attrs = %{
-      "path" => show_path,
-      "title" => title,
-      "description" => description,
-      "orientation" => orientation,
-      "file_type" => content_type
-    }
-
-    {:ok, attrs}
-  end
-
-  defp parse_upload_params(_, _) do
-    {:error, :file_not_uploaded}
-  end
 end
