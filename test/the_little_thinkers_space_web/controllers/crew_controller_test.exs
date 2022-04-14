620c0119bbb92c7abaa0c44b2d8775cc65cf9053
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-04-14 15:55:32 -0700

    add crew controller tests

---
+++ test/the_little_thinkers_space_web/controllers/crew_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule TheLittleThinkersSpaceWeb.CrewControllerTest do
+  use TheLittleThinkersSpaceWeb.ConnCase
+  import TheLittleThinkersSpace.AccountsFixtures
+  alias TheLittleThinkersSpace.Accounts
+
+  setup do
+    %{user: user_fixture()}
+  end
+
+  describe "index/2" do
+
+    test "redirects to login when user is not logged in", %{conn: conn} do
+      conn = get(conn, Routes.crew_path(conn, :index))
+
+      assert html_response(conn, 302) =~
+               "<html><body>You are being <a href=\"/users/log_in\">redirected</a>.</body></html>"
+    end
+
+    test "lists all followers of a user when user is logged in", %{conn: conn, user: user} do
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get(Routes.crew_path(conn, :index))
+
+      assert html_response(conn, 200) =~ "The Crew"
+    end
+  end
+end
