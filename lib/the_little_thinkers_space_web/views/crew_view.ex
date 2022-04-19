fea0af6fe9fbf337fa1d02dd0c33c558588ea8e6
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-04-19 13:11:02 -0700

    add link to crew profile - where available - to crew index

--- lib/the_little_thinkers_space_web/views/crew_view.ex
+++ lib/the_little_thinkers_space_web/views/crew_view.ex
@@ -1,3 +1,15 @@
 defmodule TheLittleThinkersSpaceWeb.CrewView do
   use TheLittleThinkersSpaceWeb, :view
+
+  alias TheLittleThinkersSpace.Accounts
+
+  def display_profile_link(crew_id) do
+    case Accounts.get_user_profile(crew_id) do
+      %Accounts.Profile{} = profile ->
+        profile_id = to_string(profile.id)
+        link("SHOW", to: "/profiles/" <> profile_id)
+     _ -> content_tag(:a, "NOTHING TO SEE YET")
+
+   end
+  end
 end
